702e122e7141f6924db59645f623573e2a761cea
akryga <akryga@gmail.com>
2024-02-06 10:54:29 +0300

    all works with styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormEditor } from '@bpmn-io/form-js';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'form-js-app';
+  @ViewChild('form', { static: true }) private elForm: ElementRef<HTMLDivElement> | undefined;
+
+  formEditor: FormEditor|undefined;
+  
+  ngOnInit(): void {
+    this.formEditor = new FormEditor({
+      container: this.elForm?.nativeElement
+    });
+    fetch("assets/form.json").
+      then((r)=>{return r.json()}).
+      then((j) => {this.formEditor?.importSchema(j);});
+  }
 }
